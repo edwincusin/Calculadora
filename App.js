d3cfd58a403e51fb82151776a0ca9d1ccd3a3ab2
EDWINCSN <c.edwin32@hotmail.com>
2026-03-01 22:34:43 -0500

    reto completo 22 : calculadora

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  //VARIABLES DE ESTADO
+  const [numero1,setNumero1]=useState("");
+  const [numero2,setNumero2]=useState("");
+  const [resultado, setResultado]=useState(0);  
+  
+  let convertidor=(dato)=>{
+    return parseFloat(dato) || 0; // si es otro valor que no sea numerico toma el valor 0 por defaul
+  };
+
+  let sumar=()=>{
+    let total=convertidor(numero1)+convertidor(numero2);
+    setResultado(total.toFixed(2));
+  };
+
+  let restar=()=>{
+    let total=convertidor(numero1)-convertidor(numero2);
+    setResultado(total.toFixed(2));
+  };
+
+  let multiplicar=()=>{
+    let total=convertidor(numero1)*convertidor(numero2);
+    setResultado(total.toFixed(2));
+  };
+
   return (
     <View style={styles.container}>
       <Text>Reto 22 : CALCULADORA </Text>
+      <TextInput
+        style={styles.cajatexto} 
+        value={numero1}
+        onChangeText={(dato)=>{
+          setNumero1(dato);
+        }}
+      />
+      <TextInput
+        style={styles.cajatexto}
+        value={numero2}
+        onChangeText={(dato)=>{
+          setNumero2(dato);
+        }}
+      />
+      <Text style={styles.lblResultado} >Resultado: {resultado}</Text>
+      <Button
+        title='Sumar'
+        onPress={sumar}
+      />
+      <Button
+        title='restar'
+        onPress={restar}
+      />
+      <Button
+        title='multiplicar'
+        onPress={multiplicar}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +70,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cajatexto:{
+    borderColor: 'black',
+    borderWidth:2,
+    padding:20,
+    margin:5,
+    fontSize:15
+  },
+  lblResultado:{
+    padding:20,
+    margin:5,
+    fontSize:20
+  }
 });
